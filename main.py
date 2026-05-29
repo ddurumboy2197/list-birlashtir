6fbc47154076946cf0ddf4b8e9f0684d9e19ad8d
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-05-29 23:22:33 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,301 @@
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan_list)
+```
+
+```python
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+birlashtirilgan_list = list1 + list2
+print(birlashtirilgan
